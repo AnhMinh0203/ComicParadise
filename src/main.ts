629960050e725f0531143a97c6fb0d9919789784
AnhMinh0203 <Leanhminh0203@gmail.com>
2025-07-15 16:07:51 +0700

    refactor: clean code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
-// /// <reference types="@angular/localize" />
-
-// import { bootstrapApplication } from '@angular/platform-browser';
-
-// import { AppComponent } from './app/app.component';
-// import { appConfig } from './app/app.config';
-
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch(err => console.error(err));
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -17,9 +7,9 @@ import { appConfig } from './app/app.config';
 import { provideHttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 bootstrapApplication(AppComponent, {
-  ...appConfig, // giữ lại các cấu hình đã có trong appConfig
+  ...appConfig, 
   providers: [
-    ...appConfig.providers, // kết hợp providers đã có
+    ...appConfig.providers, 
     provideHttpClient(),
     importProvidersFrom(RouterModule.forRoot(routes)),
     MessageService
